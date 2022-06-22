33b252e361653cbab3861516d67f5457c797231c
Vida Kamalifar <vida.kamalifar@lovys.com>
2022-06-22 12:38:59 +0100

    Add retrofit API

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "7b14bbc428094446ab0062004ed44d27"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
